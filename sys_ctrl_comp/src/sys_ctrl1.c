a49167259cfbed93f4b342b15b0ff84390a47af9
Pablo <practice@practice.myguest.virtualbox.org>
2026-07-28 09:45:54 -0400

    KAN-12: Adding this new additional API just for demonstration. This will not be called for a few reasons. APB is used by default, which is fine for demonstrations. I also want to add a default configuration. Users may change these if they wish

--- sys_ctrl_comp/src/sys_ctrl1.c
+++ sys_ctrl_comp/src/sys_ctrl1.c
@@ -5,3 +5,8 @@ void Sysctrl_configure_gpio_port(Sysctrl_GpioPort_t gpio_port, bool enable)
 {
 	SYSCTR32_RW(RCGCGPIO) = enable << gpio_port;
 }
+
+void Sysctrl_configure_gpio_bus(Sysctrl_GpioPort_t gpio_port, Sysctrl_Bus_t bus)
+{
+	SYSCTR32_RW(GPIOHBCTL) = bus << gpio_port;
+}
