ca8aafafff94b1facc2ed8cac872b1c640e928ee
Pablo <practice@practice.myguest.virtualbox.org>
2026-08-01 19:48:50 -0400

    KAN-12: Used linker errors to find newlib source files trying to use the sys calls and put in the correct signatures. Added implementations

--- hal/newlib_syscall/syscall.c
+++ hal/newlib_syscall/syscall.c
@@ -11,28 +11,45 @@ int _close(int fd)
 	return -1;
 }
 
-void _exit()
+void _exit(int code)
 {
 	// No implementation
 	while(1) ;
 }
 
-int _lskeek(int file, int ptr, int dir)
+int _lskeek(int fd, _off_t pos, int whence)
 {
 	// No filesystem implementation
 	return 0;
 }
 
-int read(int file, char *ptr, int len)
+int _read(int fd, void *buf, size_t cnt)
 {
 	// No filesystem implementation
 	return 0;
 }
 
-int write(int file, char *ptr, int len)
+int _write(int fd, const void *buf, size_t cnt)
 {
 	// No filesystem implementation
 	return 0;
 }
 
-int sbrk(
+// Used for memory allocation. Implementation from document above
+int _sbrk(ptrdiff_t incr)
+{
+  extern char _end;		/* Defined by the linker */
+  static char *heap_end;
+  char *prev_heap_end;
+ 
+  if (heap_end == 0) {
+    heap_end = &_end;
+  }
+  prev_heap_end = heap_end;
+  if (heap_end + incr > stack_ptr) {
+    abort ();
+  }
+
+  heap_end += incr;
+  return (caddr_t) prev_heap_end;
+}
