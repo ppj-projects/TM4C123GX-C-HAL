df97bf2a70db5bfa6646d5f75697626fe5ef8be2
Pablo <practice@practice.myguest.virtualbox.org>
2026-08-05 12:02:10 -0400

    KAN-12: Fixed comments and indents. Added functions to deal with each section separately for the constructors

--- bsp/startup_dir/startup_tm4c123.c
+++ bsp/startup_dir/startup_tm4c123.c
@@ -208,113 +208,119 @@ void I2c3_ISR()
 
 
 // CMSIS
-typedef void (*isr_t)(void);
+typedef void (*Isr_t)(void);
 
 __attribute__((section(".isr_vectors")));
-const volatile isr_t vector_table[155] =
+const volatile Isr_t vector_table[155] =
 {
-    (isr_t)&_stack_start, // sp // stack pointer defined in linker script. At top of SRAM
+	// stack pointer defined in linker script. At top of SRAM
+	(isr_t)&_stack_start,
 
 	/* system exceptions */
-    Reset_ISR, // RESET
-    Nmi_ISR// NON-MASKABLE INTERRUPT NMI
-    Hardfault_ISR// HARD FAULT
-    MemMan_ISR// MEMORY MANAGEMENT
-    Busfault_ISR // BUS FAULT
-    Usagefault_ISR// USAGE FAULT
-    0, // RESERVED (0)
-    Svc_ISR, // SVCALL
-    Debugmon_ISR, // DEBUG MONITOR
-    0, // RESERVED (0)
-    Pendsv_ISR, // PENDSV
-    Systick_ISR, // SYSTICK
+	Reset_ISR, 		// RESET
+	Nmi_ISR,		// NON-MASKABLE INTERRUPT NMI
+	Hardfault_ISR,		// HARD FAULT
+	MemMan_ISR,		// MEMORY MANAGEMENT
+	Busfault_ISR,		// BUS FAULT
+	Usagefault_ISR,	// USAGE FAULT
+	0, 			// RESERVED (0)
+	Svc_ISR, 		// SVCALL
+	Debugmon_ISR, 		// DEBUG MONITOR
+	0, 			// RESERVED (0)
+	Pendsv_ISR, 		// PENDSV
+	Systick_ISR, 		// SYSTICK
 
 	/* interrupts */
-    GpioA_ISR, // GPIOA
-    GpioB_ISR, // GPIOB
-    GpioC_ISR, // GPIOC
-    GpioD_ISR, // GPIOD
-    GpioE_ISR, // GPIOE
-    Uart0_ISR, // UART0
-    Uart1_ISR, // UART1
-    Ssi0_ISR, // SSI0
-    I2c0_ISR, // I2C0
-    Unused_ISR, // PWM0 FAULT
-    Unused_ISR, // PWM0 GENERATOR 0
-    Unused_ISR, // PWM0 GENERATOR 1
-    Unused_ISR, // PWM0 GENERATOR 2
-    Unused_ISR, // QEI0
-    Unused_ISR, // ADC0 SEQUENCE 0
-    Unused_ISR, // ADC0 SEQUENCE 1
-    Unused_ISR, // ADC0 SEQUENCE 2
-    Unused_ISR, // ADC0 SEQUENCE 3
-    Unused_ISR, // WATCHDOG TIMER 0 AND 1
-    Unused_ISR, // TIMER 0A
-    Unused_ISR, // TIMER 0B
-    Unused_ISR, // TIMER 1A
-    Unused_ISR, // TIMER 1B
-    Unused_ISR, // TIMER 2A
-    Unused_ISR, // TIMER 2B
-    Unused_ISR, // ANALOG COMPARATOR 0
-    Unused_ISR, // ANALOG COMPARATOR 1
-    0, // RESERVED[1]
-    Unused_ISR, // SYSTEM CONTROL
-    Unused_ISR, // FLASH MEMORY CONTROL AND EEPROM CONTROL
-    GpioF_ISR, // GPIOF
-    0, 0, // RESERVED[2]
-    Uart2_ISR, // UART2
-    Ssi1_ISR, // SSI1
-    Unused_ISR, // TIMER 3A
-    Unused_ISR, // TIMER 3B
-    I2c1_ISR, // I2C1
-    Unused_ISR, // QEI1
-    Can0_ISR, // CAN0
-    Can1_ISR, // CAN1
-    0, 0, // RESERVED[2]
-    Unused_ISR, // HIBERNATION MODULE
-    Usb_ISR, // USB
-    Unused_ISR, // PWM GENERATOR 3
-    Unused_ISR, // uDMA SOFTWARE
-    Unused_ISR, // uDMA ERROR
-    Unused_ISR, // ADC1 SEQUENCE 0
-    Unused_ISR, // ADC1 SEQUENCE 1
-    Unused_ISR, // ADC1 SEQUENCE 2
-    Unused_ISR, // ADC1 SEQUENCE 3
-    0, 0, 0, 0, 0,// RESERVED[5]
-    Ssi2_ISR, // SSI2
-    Ssi3_ISR, // SSI3
-    Uart3_ISR, // UART3
-    Uart4_ISR, // UART4
-    Uart5_ISR, // UART5
-    Uart6_ISR, // UART6
-    Uart7_ISR, // UART7
-    0, 0, 0, 0, // RESERVED[4]
-    I2c2_ISR, // I2C2
-    I2c3_ISR, // I2C3
-    Unused_ISR, // TIMER 4A
-    Unused_ISR, // TIMER 4B
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // RESERVED[20]
-    Unused_ISR, // TIMER 5A
-    Unused_ISR, // TIMER 5B
-    Unused_ISR, // TIMER 0A (32/64-BIT)
-    Unused_ISR, // TIMER 0B (32/64-BIT)
-    Unused_ISR, // TIMER 1A (32/64-BIT)
-    Unused_ISR, // TIMER 1B (32/64-BIT)
-    Unused_ISR, // TIMER 2A (32/64-BIT)
-    Unused_ISR, // TIMER 2B (32/64-BIT)
-    Unused_ISR, // TIMER 3A (32/64-BIT)
-    Unused_ISR, // TIMER 3B (32/64-BIT)
-    Unused_ISR, // TIMER 4A (32/64-BIT)
-    Unused_ISR, // TIMER 4B (32/64-BIT)
-    Unused_ISR, // TIMER 5A (32/64-BIT)
-    Unused_ISR, // TIMER 5B (32/64-BIT)
-    Unused_ISR, // SYSTEM EXCEPTION (IMPRECISE)
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // RESERVED[27]
-    Unused_ISR, // PWM1 GENERATOR 0
-    Unused_ISR, // PWM1 GENERATOR 1
-    Unused_ISR, // PWM1 GENERATOR 2
-    Unused_ISR, // PWM1 GENERATOR 3
-    Unused_ISR  // PWM1 FAULT
+	GpioA_ISR, 		// GPIOA
+	GpioB_ISR, 		// GPIOB
+	GpioC_ISR, 		// GPIOC
+	GpioD_ISR, 		// GPIOD
+	GpioE_ISR, 		// GPIOE
+	Uart0_ISR, 		// UART0
+	Uart1_ISR, 		// UART1
+	Ssi0_ISR, 		// SSI0
+	I2c0_ISR, 		// I2C0
+	Unused_ISR, 		// PWM0 FAULT
+	Unused_ISR, 		// PWM0 GENERATOR 0
+	Unused_ISR, 		// PWM0 GENERATOR 1
+	Unused_ISR,  		// PWM0 GENERATOR 2
+	Unused_ISR,  		// QEI0
+	Unused_ISR, 		// ADC0 SEQUENCE 0
+	Unused_ISR,  		// ADC0 SEQUENCE 1
+	Unused_ISR,  		// ADC0 SEQUENCE 2
+	Unused_ISR,  		// ADC0 SEQUENCE 3
+	Unused_ISR,  		// WATCHDOG TIMER 0 AND 1
+	Unused_ISR,  		// TIMER 0A
+	Unused_ISR,  		// TIMER 0B
+	Unused_ISR,  		// TIMER 1A
+	Unused_ISR,  		// TIMER 1B
+	Unused_ISR,  		// TIMER 2A
+	Unused_ISR,  		// TIMER 2B
+	Unused_ISR,  		// ANALOG COMPARATOR 0
+	Unused_ISR,  		// ANALOG COMPARATOR 1
+	0,  			// RESERVED[1]
+	Unused_ISR,  		// SYSTEM CONTROL
+	Unused_ISR,  		// FLASH MEMORY CONTROL AND EEPROM CONTROL
+	GpioF_ISR,  		// GPIOF
+	0, 0,  		// RESERVED[2]
+	Uart2_ISR, 		// UART2
+	Ssi1_ISR,  		// SSI1
+	Unused_ISR,  		// TIMER 3A
+	Unused_ISR,  		// TIMER 3B
+	I2c1_ISR,  		// I2C1
+	Unused_ISR,  		// QEI1
+	Can0_ISR, 		// CAN0
+	Can1_ISR,  		// CAN1
+	0, 0,  		// RESERVED[2]
+	Unused_ISR,  		// HIBERNATION MODULE
+	Usb_ISR,  		// USB
+	Unused_ISR,  		// PWM GENERATOR 3
+	Unused_ISR,  		// uDMA SOFTWARE
+	Unused_ISR,  		// uDMA ERROR
+	Unused_ISR,  		// ADC1 SEQUENCE 0
+	Unused_ISR,  		// ADC1 SEQUENCE 1
+	Unused_ISR, 		// ADC1 SEQUENCE 2
+	Unused_ISR,  		// ADC1 SEQUENCE 3
+	0, 0, 0, 0, 0, 	// RESERVED[5]
+	Ssi2_ISR,  		// SSI2
+	Ssi3_ISR,  		// SSI3
+	Uart3_ISR,  		// UART3
+	Uart4_ISR,  		// UART4
+	Uart5_ISR,  		// UART5
+	Uart6_ISR,  		// UART6
+	Uart7_ISR,  		// UART7
+	0, 0, 0, 0,  		// RESERVED[4]
+	I2c2_ISR,  		// I2C2
+	I2c3_ISR,  		// I2C3
+	Unused_ISR,  		// TIMER 4A
+	Unused_ISR,  		// TIMER 4B
+	0, 0, 0, 0, 0, 0, 0, 	// RESERVED[20]
+	0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 
+	Unused_ISR,   		// TIMER 5A
+	Unused_ISR,   		// TIMER 5B
+	Unused_ISR,   		// TIMER 0A (32/64-BIT)
+	Unused_ISR,   		// TIMER 0B (32/64-BIT)
+	Unused_ISR,   		// TIMER 1A (32/64-BIT)
+	Unused_ISR,   		// TIMER 1B (32/64-BIT)
+	Unused_ISR,   		// TIMER 2A (32/64-BIT)
+	Unused_ISR,   		// TIMER 2B (32/64-BIT)
+	Unused_ISR,   		// TIMER 3A (32/64-BIT)
+	Unused_ISR,   		// TIMER 3B (32/64-BIT)
+	Unused_ISR,   		// TIMER 4A (32/64-BIT)
+	Unused_ISR,   		// TIMER 4B (32/64-BIT)
+	Unused_ISR,   		// TIMER 5A (32/64-BIT)
+	Unused_ISR,   		// TIMER 5B (32/64-BIT)
+	Unused_ISR,   		// SYSTEM EXCEPTION (IMPRECISE)
+	0, 0, 0, 0, 0, 0, 0, 	// RESERVED[27]
+	0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0,
+	Unused_ISR, 		// PWM1 GENERATOR 0
+	Unused_ISR, 		// PWM1 GENERATOR 1
+	Unused_ISR, 		// PWM1 GENERATOR 2
+	Unused_ISR, 		// PWM1 GENERATOR 3
+	Unused_ISR 		 // PWM1 FAULT
 }
 
 
@@ -336,7 +342,9 @@ void startup()
 	init_static_data()
 
 	// Initialize constructor... but this is C?
-	init_static_ctors()
+	init_preinit_array()
+	init_init_array()
+	init_ctors()
 
 	// Start main
 	main();
@@ -370,10 +378,33 @@ void init_static_data()
 	}
 }
 
+typedef void (*Preinit_array_t)(void);
+extern Preinit_array_t _preinit_start;
+extern Preinit_array_t _preinit_end;
 
 // Need pointers to ctors and init_array
-void init_static_ctors()
+void init_preinit_array()
 {
+
+}
+
+typedef void (*Init_array_t)(void);
+extern Init_array_t _init_array_start;
+extern Init_array_t _init_array_end;
+
+void init_init_array()
+{
+
+}
+
+typedef void (*Ctors_t)(void);
+extern Ctors_t _ctors_start;
+extern Ctors_t _ctors_end;
+
+void init_ctors()
+{
+
+}
 
 }
 
