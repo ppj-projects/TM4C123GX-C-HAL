c2a6dd9cd945d9fc7c151a7b516008ceec26905d
Pablo <practice@practice.myguest.virtualbox.org>
2026-08-05 13:28:17 -0400

    KAN-12: Updating the sbrk function with the linker script labels I created before for the heap and stack

--- hal/clib/newlib_syscall/syscall.c
+++ hal/clib/newlib_syscall/syscall.c
@@ -26,33 +26,47 @@ int _lseek(int fd, _off_t pos, int whence)
 int _read(int fd, void *buf, size_t cnt)
 {
 	// No filesystem implementation
+	// Can be used to read from UART, but it's not implemented
 	return 0;
 }
 
 int _write(int fd, const void *buf, size_t cnt)
 {
 	// No filesystem implementation
+	// Can be used to write to UART, but it's not implemented
 	return 0;
 }
+
+extern uint32_t _stack_start;
+extern uint32_t _heap_start;
 
 // Used for memory allocation. Implementation from document above
 int _sbrk(ptrdiff_t incr)
 {
-  extern char _end;		/* Defined by the linker */
-  static char *heap_end;
-  char *prev_heap_end;
+  static uint32_t *heap_end;
+  uint32_t *prev_heap_end;
+  uint32_t stack_ptr;
  
-  if (heap_end == 0) {
-    heap_end = &_end;
+  // Nullptr
+  if (heap_end == 0) 
+  {
+	// At init heap is empty. Start of heap is end of heap.
+	// Initialize it  
+	heap_end = &_heap_start;
   }
   prev_heap_end = heap_end;
   
-  // Not sure where stack_ptr is meant to be defined. 
-  // Commenting out for now. But this is meant to be a guard.
-  //if (heap_end + incr > stack_ptr) {
-  //  abort ();
-  //}
+  // Use inline assembly to find the current top of stack
+  __asm volatile ("mrs %0, msp" : "=r"(stack_ptr));
+  
+  if (heap_end + incr > stack_ptr) 
+  {
+    // Heap collided into stack. Do not allocate memory. Return
+    return -1;
+  }
 
   heap_end += incr;
+  
+  // Return the old end of heap
   return (int)prev_heap_end;
 }
